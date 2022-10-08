a4e401c6b2d6fed55434f474f9f16c164de3ec9a
ArihitoMatsuda <arihito.m@gmail.com>
2022-10-08 11:56:43 +0900

    add: Mail signIn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import NextAuth from 'next-auth'
-import EmailProvider from 'next-auth/provider/email'
+import EmailProvider from 'next-auth/providers/email'
 
 import { prisma } from '~/libs/prisma'
 
 const EMAIL_SERVER = process.env.EMAIL_SERVER
-const EMAIL_FROM = process.env.EMAIL_FORM
+const EMAIL_FROM = process.env.EMAIL_FROM
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET
 
 if (!EMAIL_SERVER || !EMAIL_FROM || !NEXTAUTH_SECRET) {
